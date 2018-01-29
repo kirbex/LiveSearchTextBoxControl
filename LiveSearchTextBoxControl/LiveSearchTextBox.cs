20dcde18fbca98daa1a0f2db022369016e3bf675
Yury.Kerbiczkov <yury.kerbiczkov@e-lab.icl.kazan.ru>
2018-01-29 17:55:51 +0300

    version 0.1.0.0

---
+++ LiveSearchTextBoxControl/LiveSearchTextBox.cs
@@ -0,0 +1,171 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace LiveSearchTextBoxControl
+{
+    using System.Threading.Tasks;
+    using System.Timers;
+    using System.Windows.Input;
+
+    public class LiveSearchTextBox : TextBox
+    {
+        public static readonly RoutedEvent FilterEvent = EventManager.RegisterRoutedEvent(
+            nameof(Filter),
+            RoutingStrategy.Bubble,
+            typeof(RoutedEventHandler),
+            typeof(LiveSearchTextBox));
+
+        public static readonly RoutedEvent CancelFilteringEvent = EventManager.RegisterRoutedEvent(
+            nameof(CancelFiltering),
+            RoutingStrategy.Bubble,
+            typeof(RoutedEventHandler),
+            typeof(LiveSearchTextBox));
+
+        public static readonly DependencyProperty FilterTaskProperty = DependencyProperty.Register(
+            nameof(FilterTask),
+            typeof(Task),
+            typeof(LiveSearchTextBox),
+            new FrameworkPropertyMetadata(Task.CompletedTask));
+
+        public static readonly DependencyProperty FilterCommandProperty = DependencyProperty.Register(
+            nameof(FilterCommand),
+            typeof(ICommand),
+            typeof(LiveSearchTextBox));
+
+        public static readonly DependencyProperty CancelFilteringCommandProperty = DependencyProperty.Register(
+            nameof(CancelFilteringCommand),
+            typeof(ICommand),
+            typeof(LiveSearchTextBox));
+
+        public static readonly DependencyProperty HintTextProperty = DependencyProperty.Register(
+            nameof(HintText),
+            typeof(string),
+            typeof(LiveSearchTextBox));
+
+        public static readonly DependencyProperty HintTemplateProperty = DependencyProperty.Register(
+            nameof(HintTemplate),
+            typeof(DataTemplate),
+            typeof(LiveSearchTextBox),
+            new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.AffectsRender));
+
+        private Timer keyPressedDelayTimer;
+
+        private int delayAfterPressingKeyInMilliseconds;
+
+        static LiveSearchTextBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(
+                typeof(LiveSearchTextBox),
+                new FrameworkPropertyMetadata(typeof(LiveSearchTextBox)));
+        }
+
+        public LiveSearchTextBox() => InitializeTimer();
+
+        public event RoutedEventHandler Filter
+        {
+            add => AddHandler(FilterEvent, value);
+            remove => RemoveHandler(FilterEvent, value);
+        }
+
+        public event RoutedEventHandler CancelFiltering
+        {
+            add => AddHandler(CancelFilteringEvent, value);
+            remove => RemoveHandler(CancelFilteringEvent, value);
+        }
+
+        public Task FilterTask
+        {
+            get => (Task)GetValue(FilterTaskProperty);
+            set => SetValue(FilterTaskProperty, value);
+        }
+
+        public string HintText
+        {
+            get => (string)GetValue(HintTextProperty);
+            set => SetValue(HintTextProperty, value);
+        }
+
+        public DataTemplate HintTemplate
+        {
+            get => (DataTemplate)GetValue(HintTemplateProperty);
+            set => SetValue(HintTemplateProperty, value);
+        }
+
+        public bool WaitTillTaskIsCompleted { get; set; }
+
+        public int DelayAfterPressingKeyInMilliseconds
+        {
+            get => delayAfterPressingKeyInMilliseconds;
+            set
+            {
+                if (delayAfterPressingKeyInMilliseconds == value) return;
+                delayAfterPressingKeyInMilliseconds = value;
+                keyPressedDelayTimer.Interval = value;
+            }
+        }
+
+        public ICommand FilterCommand
+        {
+            get => (ICommand)GetValue(FilterCommandProperty);
+            set => SetValue(FilterCommandProperty, value);
+        }
+
+        public ICommand CancelFilteringCommand
+        {
+            get => (ICommand)GetValue(CancelFilteringCommandProperty);
+            set => SetValue(CancelFilteringCommandProperty, value);
+        }
+
+        protected override async void OnTextChanged(TextChangedEventArgs e)
+        {
+            await OnTextChangedAsync().ConfigureAwait(true);
+            base.OnTextChanged(e);
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                Text = string.Empty;
+                RaiseFilter();
+            }
+
+            base.OnKeyDown(e);
+        }
+
+        private void InitializeTimer()
+        {
+            keyPressedDelayTimer = new Timer { AutoReset = false };
+            keyPressedDelayTimer.Elapsed += TimerElapsed;
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            RaiseFilter();
+        }
+
+        private async Task OnTextChangedAsync()
+        {
+            keyPressedDelayTimer.Stop();
+            RaiseCancelFilter();
+            if (WaitTillTaskIsCompleted && FilterTask != null) await FilterTask.ConfigureAwait(false);
+            keyPressedDelayTimer.Start();
+        }
+
+        private void RaiseFilter()
+        {
+            Dispatcher.Invoke(
+                () =>
+                    {
+                        RaiseEvent(new RoutedEventArgs(FilterEvent, this));
+                        FilterCommand?.Execute(null);
+                    });
+        }
+
+        private void RaiseCancelFilter()
+        {
+            RaiseEvent(new RoutedEventArgs(CancelFilteringEvent, this));
+            CancelFilteringCommand?.Execute(null);
+        }
+    }
+}
---
+++ LiveSearchTextBoxControlTest/MainWindow.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System.Windows;
+
+namespace LiveSearchTextBoxControlTest
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private readonly List<string> sourceCollection =
+            new List<string>
+                {
+                    "1",
+                    "2",
+                    "3",
+                    "4",
+                    "5",
+                    "6",
+                    "7",
+                    "8",
+                    "9",
+                    "10",
+                    "11",
+                    "12",
+                    "13",
+                    "14",
+                    "15",
+                    "16",
+                    "17",
+                    "18",
+                    "19",
+                    "20",
+                    "Hello, world!",
+                    "Heello, world!!"
+                };
+
+        private Task filterTask;
+
+        private ObservableCollection<string> collection;
+
+        private CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+
+        public MainWindow()
+        {
+            FillSourceCollection();
+            collection = new ObservableCollection<string>(sourceCollection);
+            InitializeComponent();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<string> Collection
+        {
+            get => collection;
+            set
+            {
+                collection = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Task FilterTask
+        {
+            get => filterTask;
+            set
+            {
+                filterTask = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string FilterText { get; set; }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void FillSourceCollection()
+        {
+            Random random = new Random();
+            for (int i = 0; i < 1_000_000; i++) sourceCollection.Add(random.Next(1_000_000).ToString());
+        }
+
+        private void LiveSearchTextBox_OnFilter(object sender, RoutedEventArgs e)
+        {
+            FilterTask = FilterTask?.ContinueWith(_ => Filter()) ?? Task.Run(() => Filter());
+        }
+
+        private void Filter()
+        {
+            if (string.IsNullOrEmpty(FilterText))
+            {
+                Collection = new ObservableCollection<string>(sourceCollection);
+                return;
+            }
+
+            cancellationTokenSource = new CancellationTokenSource();
+            var tempCollection = new ObservableCollection<string>();
+            foreach (var str in sourceCollection)
+            {
+                if (cancellationTokenSource.Token.IsCancellationRequested) return;
+
+                if (str.Contains(FilterText)) tempCollection.Add(str);
+            }
+
+            Collection = tempCollection;
+        }
+
+        private void LiveSearchTextBox_OnCancelFiltering(object sender, RoutedEventArgs e)
+        {
+            cancellationTokenSource.Cancel();
+        }
+    }
+}
